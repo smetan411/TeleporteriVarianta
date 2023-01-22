70d7cdb3f24c55dc265f4c09f7f4a2c540f01d6a
smetan411 <anna.smetanova@atlas.cz>
2023-01-22 10:53:54 +0100

    dokonceni

---
+++ src/main/java/hra/teleporteri/listenery/TeleporteriNesmrtelnost.java
@@ -0,0 +1,39 @@
+package hra.teleporteri.listenery;
+
+import hra.teleporteri.Teleporteri;
+import hra.vybava.MecNaTeleportery;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import static hra.vybava.MecNaTeleportery.MEC_NA_TELEPORTERY;
+
+public class TeleporteriNesmrtelnost {
+    @EventHandler
+    public void nastavNesmrtelnost(EntityDamageByEntityEvent udalost) {
+
+       var utocnik = (Player) udalost.getDamager();
+
+        if ((Teleporteri.DUCH.getJmeno().equals(udalost.getEntity().getCustomName())) &&
+                (!(MEC_NA_TELEPORTERY.equals(utocnik.getInventory().getItemInMainHand().getItemMeta().getDisplayName())))) {
+            udalost.setCancelled(true);
+        }
+        else if ((Teleporteri.GOLEM.getJmeno().equals(udalost.getEntity().getCustomName())) &&
+                (!(MEC_NA_TELEPORTERY.equals(utocnik.getInventory().getItemInMainHand().getItemMeta().getDisplayName())))) {
+            udalost.setCancelled(true);
+        }
+        else if ((Teleporteri.LUCISTNIK.getJmeno().equals(udalost.getEntity().getCustomName())) &&
+                (!(MEC_NA_TELEPORTERY.equals(utocnik.getInventory().getItemInMainHand().getItemMeta().getDisplayName())))) {
+            udalost.setCancelled(true);
+        }
+        else if ((Teleporteri.OHNIVAK.getJmeno().equals(udalost.getEntity().getCustomName())) &&
+                (!(MEC_NA_TELEPORTERY.equals(utocnik.getInventory().getItemInMainHand().getItemMeta().getDisplayName())))) {
+            udalost.setCancelled(true);
+        }
+        else if ((Teleporteri.VIDLAK.getJmeno().equals(udalost.getEntity().getCustomName())) &&
+                (!(MEC_NA_TELEPORTERY.equals(utocnik.getInventory().getItemInMainHand().getItemMeta().getDisplayName())))) {
+            udalost.setCancelled(true);
+        }
+
+
+    }
+}
